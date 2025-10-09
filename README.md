3e1b1f363069039e627d8b0c8b956fa913906aa2
Jerson-Cortes <jersoncortes.work@proton.me>
2025-10-08 23:52:43 -0600

    Imports data correctly. Needs to import data into cvs

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
+### Stellar-distribution-script
+
+A simple script to get and distill data for a 3D render of the galaxy using astroquery with Gaia.
+
 ## Installation
 
 Set environment
 ```
 python3 -m venv venv
 source venv/bin/activate
-
 ```
 
 Install necessary packages
--- script.py
+++ script.py
@@ -1,1 +1,14 @@
 from astroquery.gaia import Gaia
+import pandas as pd
+
+job = Gaia.launch_job_async("""
+SELECT source_id, ra, dec, phot_g_mean_mag, parallax, pmra, pmdec
+FROM gaiadr3.gaia_source
+WHERE phot_g_mean_mag < 8
+AND parallax > 0.1
+AND ABS(b) < 30
+""")
+
+gaia_tbl = job.get_results()
+
+print(gaia_tbl[:5])
